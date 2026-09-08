3c1c4c7bbf5b0b3fee7ac206c56eee934113c0fd
sksinghvlogger-del <sksinghvlogger@gmail.com>
2026-09-08 09:56:15 +0530

    first

---
+++ dhhf.py
@@ -0,0 +1,252 @@
+
+# p_name="orange"
+# price=30
+# quantity=5
+# print(f"P: {p_name}, Pr: {price}, Q: {quantity}")
+
+
+#s_M= int(input("Enter"))
+#if s_M >=40:
+ #  print("sM: ", s_M)
+#else: 
+ # print("Low Marks")
+
+#i=100
+#while i>=1:
+    # print(i)
+    # i-=1
+
+# nu=[1,2,3,4,5]
+# id=0
+# while id<len(nu):
+#     print(id)
+#     id+=1
+
+
+# print("Hello World")
+# print('Hello World')
+
+# print('My Clg is rungta\nMy Classroom is Sa02\nMy Department is CSE\nMy Name is Avtar')4
+
+
+# a=20
+
+# a=25
+# b=a
+# print(b)
+
+
+# name= "Avtar"-+-ge=21
+# salary= 250000
+
+# print(f"Name: {name}, Age: {age}, Salary: {salary}")
+
+# a=45/2
+# b=45//2
+# print(a)
+# print(b)
+
+# a=39
+# b=438
+# print('My First Num Value is:',a,'\n','My Second Num value is:',b,'\n','Sum:', a+b)
+    
+# a=5
+# b=3.5
+# sum=a+b
+# print(int(sum))
+
+# a="abd"
+# a=(a)
+# print(a)
+
+
+# a=int(input("Enter First Num: "))
+# b=int(input("Enter Second Num: "))
+
+# sum=a+b
+# print(sum)
+
+
+# a=input("Enter First Name: ")
+# print('Welcome', a)
+# age=int(input("Enter Your Age: "))
+# print('Age is', age)
+
+
+# w.a.p to input two integer numbers a and b. Print true if a is greater than or equal to b. If not print false.
+# w.a.p to enter three floating point numbers and print avg.....
+# write a program to get input side of a square and calculate its area (s*s)
+
+
+# s=int (input("Enter Side of Square: "))
+# print("Area of Square is: ", s*s)
+
+
+
+# a=input("Enter First Num: ")
+# b=input("Enter Second Num: ")
+# c=input("Enter Third Num: ")
+# avg=(float(a)+float(b)+float(c))/3
+# print("Average is: ", avg)
+
+
+
+# a=int(input("Enter First Num: "))
+# b=int(input("Enter Second Num: "))
+# print(a>=b)
+
+
+# from xmlrpc.client import boolean
+
+
+# age=int(input("Enter your age: "))
+# member=input
+
+# if age>=60:
+#     if member:
+#         print("You are eligible for 50% discount")
+#     else:
+#         print("You are eligible for 25% discount")
+# else: 
+#     print("You are not eligible for discount")
+
+# take three number from the user and fdind largest number using conditional statement
+# take a year as a input and check whether it leap year or not
+# take the account balancce  and withdrawl amount and check wheter  the amount is multiple of 100, if both condition are satified then print the remaining balance otherwise print error message
+# take marks and annual family income as input and determine the scholarship eligibility i) marks greatweer than 90 and income less than 500000 should get 50% scholarship ii) marks greater than =80
+# and income less than 500000 should get 30% scholarship iii) marks greater than =70 and income less than 300000 should get 20% scholarship iv) otherwise no scolarship
+
+
+# a=int(input("Enter No: "))
+# b=int(input("Enter No: "))
+# c=int(input("Enter No: "))
+
+# if a>=b and a>=c:
+#     print("A is Largest No.")
+# elif b>=a and b>=c:
+#    print ("B is greater")
+# else:
+#     print("C is greater")
+
+
+
+# year=int(input("Eneter year"))
+# if(year%4==0 and year%100!=0) or (year%400==0):
+#     print("Leap Year")
+# else:
+#     print("It's not a leap year")
+
+
+# Ab=int(input("enter: "))
+# if(Ab%100==0):
+#     print("Amount is multiple of 100")
+# else:
+
+# write a propgram to take input as a colour from the user and print the number of character
+# colour=input("Enter Colour: ")
+# print("N0 of char in the colour name:", len(colour))\
+
+# str="Rungta University"
+# print(str[5])
+
+
+# when we slice the array then the start point include but the end point is not include.
+
+# wap to take input of any spelling from 1-10 and print the occurence of letter "e" 
+
+# str="Rungta University"
+# print(str.count("e"))
+
+# a = [2,4,5,6,3,7,6]
+# # print(a)
+# # print(a[1:3])
+# a.append(4)
+# a.sort()
+# a.sort(reverse=True)
+# a.reverse()
+# a.remove(3)
+# a.pop(2)
+
+
+                      # wap to ask the user to enter their 4 favourite movies and store in a list and print it.
+# movie=[]
+# movie.append(input("Enter 1 Favourite movie"))
+# movie.append(input("Enter 2 Favourite movie"))
+# movie.append(input("Enter 3 Favourite movie"))
+# movie.append(input("Enter 4 Favourite movie"))
+# print("Your favourite movies are:", movie)
+
+#                             wap to check given list is palindrome or not
+
+# list=[1,2,1]
+# copy_list=list.copy()
+# copy_list.reverse()
+# if list==copy_list:
+#     print("The list is a palindrome.")
+# else:
+#     print("The list is not a palindrome.")
+
+# list=['m','o','m']
+# copy_list=list.copy()
+# copy_list.reverse()
+# if list==copy_list:
+#     print("The list is a palindrome.")
+# else:
+#     print("The list is not a palindrome.")
+                     
+                                              #Tuple
+
+# tup=(1,2,3)
+# print(tup)
+# print(type(tup))
+# print(tup[1])
+# tup1=(2,4,76,3,5,2)
+# tup1.index(4)
+# print(tup1.count(2))
+# print(tup1)
+
+
+# print length tuple and the index position for great b
+
+# grade=('c','b','a','b','c')
+# print(len(grade))
+# print(grade.index('c'))
+# print(grade.count('b'))
+
+# list1=['c','s','a','b','c']
+# list1.sort()
+# print(list1)
+
+                            # Dictionary
+
+# info={
+#     "Name" : "Avtar",
+#     "ERP"  : "11375",
+#     "City" : "Bhilai",
+#     "Marks": [89,89,78,87,99]
+# }
+
+# info.keys()
+# info.values()
+# info.items()
+# info.update({"Course":"Btech"})
+# print(info)
+# print(type(info))
+# print(info["Name"])
+# info["Course"]="Btech"
+# print(info)
+# disc={}
+# print(disc)
+# disc["Name"]="Avtar"
+# print(disc)
+
+# Store the following word meaning in the python dictionary or table :"piece of furniture",
+#                                                                    "list of facts",
+#                                                              cat: " a small animal",
+
+
+new={
+    "table":["piece of furniture",
+            "list of facts"],
+            "cat":"a small animal"}
+print(new)
